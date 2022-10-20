27ca778117ac069d85d1359d0c6212130ca5c072
GeydersonC <116230345+GeydersonC@users.noreply.github.com>
2022-10-20 00:53:13 +0000

    Aprendi a criar variáveis e exibi-las na tela

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 # 
 #print() = comando de saida
 print("Alo mundo!")
+
+# Quando quiser guardar uma String!(frase)
+nome = "Geyderson Soares Costa"
+# Quando quiser guardar um numero inteiro
+idade = 22
+
+#Exibir o meu nome (que está dentro da variavel nome)
+print(nome)
+
+
+'''
+Comentario em bloco
+
+'''
